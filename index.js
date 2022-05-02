44665cca4fd618663ec46e683c089984d214dcc8
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-02 16:29:24 +0200

    Added a simple player and enemy.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// some initial setup
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
@@ -6,9 +7,62 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
 
+// Sprite class
 class Sprite {
-    constructor(position) {
-        this.position = position;
+  // these curly brackets make it an object. now the order doesn't matter and they are optional
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
     }
+  }
 }
+
+const player = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 10,
+  },
+});
+
+// main loop
+function animate() {
+  window.requestAnimationFrame(animate); // this creates and infinte loop
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+}
+animate();
